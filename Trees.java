d967b4785a324fdd374ab3870c608cbe9e5fdcf6
Your Namegi <mirov21@outlook.com>
2024-07-24 21:11:39 -0700

    Первая задачи

---
+++ Trees.java
@@ -0,0 +1,14 @@
+public class Trees{
+    public static void main(String[] args) {
+        // Вызов метода printThreeWords
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        // Печать трех слов в столбец
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
+
