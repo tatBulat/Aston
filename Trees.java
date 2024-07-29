ac1e1fa8b4635b707bf44f82881976f655b87757
tatBulat <mirov21@outlook.com>
2024-07-29 05:37:15 -0700

    Update Trees.java

--- Trees.java
+++ Trees.java
@@ -1,12 +1,1 @@
-public class Trees{
-    public static void main(String[] args) {
-        printThreeWords();
-    }
-
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-}
 
