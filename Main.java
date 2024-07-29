b9ef6c4e4989fef183f55c75a9a0f07292fda113
tatBulat <mirov21@outlook.com>
2024-07-29 05:36:39 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // Вызов метода printThreeWords
         printThreeWords();
     }
 
     public static void printThreeWords() {
-        // Печать трех слов в столбец
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
