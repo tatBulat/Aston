8ca39fee92013db114d572084f019e3bbb21d186
tatBulat <mirov21@outlook.com>
2024-07-29 05:37:25 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,1 @@
-public class Main {
-    public static void main(String[] args) {
-        printThreeWords();
-    }
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-}
